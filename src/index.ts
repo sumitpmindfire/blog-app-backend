5a9aebe16849b8d9ea254e92a1bfd92fc40397dc
sumitpmindfire <sumitp@mindfiresolutions.com>
2023-12-05 19:11:47 +0530

    updated: port

--- src/index.ts
+++ src/index.ts
@@ -22,8 +22,8 @@ app.use(cookieParser());
 async function initializeServer() {
   try {
     await connectToDatabase();
-    app.listen(5000, () => {
-      console.log("server listening on port 5000");
+    app.listen(3000, () => {
+      console.log("server listening on port 3000");
     });
   } catch (error) {
     console.error(error);
